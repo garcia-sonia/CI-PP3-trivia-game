842675dcb5e9a19d9ef35c43f8bc6d1587a2f7b3
Sonia Garcia <soniagarciapascual@gmail.com>
2022-04-20 06:58:02 +0000

    Create 2 lists for the quiz, one list with questions and multiple choice answers and another list with the correct answer linked to each question

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+questions = [
+    "What is the most commonly broken human bone?\n(a) femur\n(b)clavicle",
+    "What vitamin is sometimes refered to as the sunshine vitamin?\n(a) vitamin A\n(b)vitamin D",
+
+keys = [
+    Key(questions[0], "a"),
+    Key(questions[1], "b")
+]
+
