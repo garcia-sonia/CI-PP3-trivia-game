3a88d45be49696dc2ffc2fd313c17f08534ae5ea
Sonia Garcia <soniagarciapascual@gmail.com>
2022-04-21 07:03:35 +0000

    Create class Key as blueprint from which objects will be created

--- run.py
+++ run.py
@@ -1,13 +1,21 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Key:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+
+
+
 questions_asked = [
     "What is the most commonly broken human bone?\n(a) femur\n(b)clavicle",
     "What vitamin is sometimes refered to as the sunshine vitamin?\n(a) vitamin A\n(b)vitamin D",
+]
 
-keys == [
-    Key(questions[0], "a"),
-    Key(questions[1], "b")
+keys = [
+    Key(questions_asked[0], "a"),
+    Key(questions_asked[1], "b")
 ]
 
 def play_quiz(keys):
@@ -15,9 +23,10 @@ def play_quiz(keys):
     '''
     score = 0
     for key in keys:
-        answer = input(question.ask)
-        if answer == question.answer:
+        answer = input(key.prompt)
+        if answer == key.answer:
             score += 1
     print("you got", score, "out of", len(questions))
+
 
 play_quiz(keys)
