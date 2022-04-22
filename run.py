cafcc0282eaa52f774731963b4d28e3fbdf8819b
Sonia Garcia <soniagarciapascual@gmail.com>
2022-04-22 06:48:19 +0000

    Create function to ask for user name and print instructions and create input field to enter password

--- run.py
+++ run.py
@@ -1,6 +1,55 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import getpass
+
+user_instructions = """Pick your decade, guess the band or the song,
+find out your score"""
+
+def introduce_quiz():
+    """
+    Initial message to introduce the quiz.
+    Input will appear for user to type in their name.
+    While loop will ensure that user does not leave
+    the name input empty.
+    Print also the instructions for the quiz
+    """
+    print('*' * 80)
+    print('')
+    print("Welcome to 'Guess the Band - Guess the Song'")
+    print('')
+    print('*' * 80)
+    print('')
+    global user_name
+    while True:
+        user_name = input("Please enter your name:\n")
+        print('')
+        if user_name == '':
+            print("Please enter a name!")
+            print('')
+        else:
+            break
+    print(f"Welcome {user_name}! Let's find out how much you know!")
+    print(user_instructions)
+    print('')
+    
+introduce_quiz()
+
+
+
+
+password = input("Please enter your password to access your coding tests")
+print("That is not your assigned password")
+quit()
+
+print("It looks like you can\'t remberber your password. Would you like a hint?")
+
+
+
+
+
+
 class Key:
     def __init__(self, prompt, answer):
         self.prompt = prompt
